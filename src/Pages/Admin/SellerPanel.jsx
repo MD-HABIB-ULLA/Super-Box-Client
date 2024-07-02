bab7c0361b87b70b959584de03dbac19b0870bab
anjumnr99 <anjumanowara99@gmail.com>
2024-07-02 15:13:39 +0600

    seller and customer panel added

---
+++ src/Pages/Admin/SellerPanel.jsx
@@ -0,0 +1,171 @@
+import Title from "../../Components/Title";
+
+
+const SellerPanel = () => {
+    return (
+        <div className="mt-10">
+            <Title title1={''} title2={'Seller Panel'}></Title>
+            <div className="overflow-x-auto mt-10">
+                <table className="table table-sm ">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Name</th>
+                            <th>Brand Name</th>
+                            <th>location</th>
+                            <th>Contact Info</th>
+                            <th>Total Product</th>
+                            <th>Last Login</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <th>1</th>
+                            <td>Cy Ganderton</td>
+                            <td>Quality Control Specialist</td>
+                            <td>Littel, Schaden and Vandervort</td>
+                            <td>Canada</td>
+                            <td>Blue</td>
+                            <td>12/16/2020</td>
+                            <td className="flex flex-row justify-start gap-1">
+                                <button className="btn btn-outline btn-info  btn-xs ">Remove</button>
+                                <button className="btn btn-outline btn-error  btn-xs ">Ban</button>
+                                <button className="btn btn-outline btn-warning btn-xs " >Suspend</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>2</th>
+                            <td>Hart Hagerty</td>
+                            <td>Desktop Support Technician</td>
+                            <td>Zemlak, Daniel and Leannon</td>
+                            <td>United States</td>
+                            <td>12/5/2020</td>
+                            <td>Purple</td>
+                            <td className="flex flex-row justify-start gap-1">
+                                <button className="btn btn-outline btn-info  btn-xs ">Remove</button>
+                                <button className="btn btn-outline btn-error  btn-xs ">Ban</button>
+                                <button className="btn btn-outline btn-warning btn-xs " >Suspend</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>3</th>
+                            <td>Brice Swyre</td>
+                            <td>Tax Accountant</td>
+                            <td>Carroll Group</td>
+                            <td>China</td>
+                            <td>8/15/2020</td>
+                            <td>Red</td>
+                            <td className="flex flex-row justify-start gap-1">
+                                <button className="btn btn-outline btn-info  btn-xs ">Remove</button>
+                                <button className="btn btn-outline btn-error  btn-xs ">Ban</button>
+                                <button className="btn btn-outline btn-warning btn-xs " >Suspend</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>4</th>
+                            <td>Marjy Ferencz</td>
+                            <td>Office Assistant I</td>
+                            <td>Rowe-Schoen</td>
+                            <td>Russia</td>
+                            <td>3/25/2021</td>
+                            <td>Crimson</td>
+                            <td className="flex flex-row justify-start gap-1">
+                                <button className="btn btn-outline btn-info  btn-xs ">Remove</button>
+                                <button className="btn btn-outline btn-error  btn-xs ">Ban</button>
+                                <button className="btn btn-outline btn-warning btn-xs " >Suspend</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>5</th>
+                            <td>Yancy Tear</td>
+                            <td>Community Outreach Specialist</td>
+                            <td>Wyman-Ledner</td>
+                            <td>Brazil</td>
+                            <td>5/22/2020</td>
+                            <td>Indigo</td>
+                            <td className="flex flex-row justify-start gap-1">
+                                <button className="btn btn-outline btn-info  btn-xs ">Remove</button>
+                                <button className="btn btn-outline btn-error  btn-xs ">Ban</button>
+                                <button className="btn btn-outline btn-warning btn-xs " >Suspend</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>6</th>
+                            <td>Irma Vasilik</td>
+                            <td>Editor</td>
+                            <td>Wiza, Bins and Emard</td>
+                            <td>Venezuela</td>
+                            <td>12/8/2020</td>
+                            <td>Purple</td>
+                            <td className="flex flex-row justify-start gap-1">
+                                <button className="btn btn-outline btn-info  btn-xs ">Remove</button>
+                                <button className="btn btn-outline btn-error  btn-xs ">Ban</button>
+                                <button className="btn btn-outline btn-warning btn-xs " >Suspend</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>7</th>
+                            <td>Meghann Durtnal</td>
+                            <td>Staff Accountant IV</td>
+                            <td>Schuster-Schimmel</td>
+                            <td>Philippines</td>
+                            <td>2/17/2021</td>
+                            <td>Yellow</td>
+                            <td className="flex flex-row justify-start gap-1">
+                                <button className="btn btn-outline btn-info  btn-xs ">Remove</button>
+                                <button className="btn btn-outline btn-error  btn-xs ">Ban</button>
+                                <button className="btn btn-outline btn-warning btn-xs " >Suspend</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>8</th>
+                            <td>Sammy Seston</td>
+                            <td>Accountant I</td>
+                            <td>O'Hara, Welch and Keebler</td>
+                            <td>Indonesia</td>
+                            <td>5/23/2020</td>
+                            <td>Crimson</td>
+                            <td className="flex flex-row justify-start gap-1">
+                                <button className="btn btn-outline btn-info  btn-xs ">Remove</button>
+                                <button className="btn btn-outline btn-error  btn-xs ">Ban</button>
+                                <button className="btn btn-outline btn-warning btn-xs " >Suspend</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>9</th>
+                            <td>Lesya Tinham</td>
+                            <td>Safety Technician IV</td>
+                            <td>Turner-Kuhlman</td>
+                            <td>Philippines</td>
+                            <td>2/21/2021</td>
+                            <td>Maroon</td>
+                            <td className="flex flex-row justify-start gap-1">
+                                <button className="btn btn-outline btn-info  btn-xs ">Remove</button>
+                                <button className="btn btn-outline btn-error  btn-xs ">Ban</button>
+                                <button className="btn btn-outline btn-warning btn-xs " >Suspend</button>
+                            </td>
+                        </tr>
+                        <tr>
+                            <th>10</th>
+                            <td>Zaneta Tewkesbury</td>
+                            <td>VP Marketing</td>
+                            <td>Sauer LLC</td>
+                            <td>Chad</td>
+                            <td>6/23/2020</td>
+                            <td>Green</td>
+                            <td className="flex flex-row justify-start gap-1">
+                                <button className="btn btn-outline btn-info  btn-xs ">Remove</button>
+                                <button className="btn btn-outline btn-error  btn-xs ">Ban</button>
+                                <button className="btn btn-outline btn-warning btn-xs " >Suspend</button>
+                            </td>
+                        </tr>
+                    </tbody>
+                    
+                </table>
+            </div>
+        </div>
+    );
+};
+
+export default SellerPanel;
--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -3,11 +3,96 @@ import { Link, NavLink, Outlet } from "react-router-dom";
 import { MdAddBusiness } from "react-icons/md";
 import { TbLogs } from "react-icons/tb";
 import { GrAtm } from "react-icons/gr";
-import { HiChatBubbleLeftRight } from "react-icons/hi2";
+import { HiChatBubbleLeftRight, HiUserGroup } from "react-icons/hi2";
 import { GrResources } from "react-icons/gr";
 import Navbar from "../../Components/Navbar";
 import { AiOutlineProduct } from "react-icons/ai";
+
+import { FaRegQuestionCircle } from "react-icons/fa";
+
 const Dashboard = () => {
+
+    const sellerNavLinks = <>
+        <li>
+            <NavLink to="/dashboard/create-shop" className={`p-2 text-lg rounded-sm`}>
+                <MdAddBusiness className="text-3xl" />
+                Create a website</NavLink>
+        </li>
+        <li>
+            <NavLink to="/dashboard/product-management" className={`p-2 text-lg rounded-sm`}>
+                <AiOutlineProduct className="text-3xl" />
+                Product Management</NavLink>
+        </li>
+        <li>
+            <NavLink to="/dashboard/blogs" className={`p-2 text-lg rounded-sm`}>
+                <TbLogs className="text-3xl" />
+                Blogs
+            </NavLink>
+        </li>
+        <li>
+            <NavLink to="/dashboard/pos" className={`p-2 text-lg rounded-sm`}>
+                <GrAtm className="text-3xl" />
+                POS
+            </NavLink>
+        </li>
+
+        <li>
+            <NavLink to="/dashboard/chat-us" className={`p-2 text-lg rounded-sm`}>
+                <HiChatBubbleLeftRight className="text-3xl" />
+                Chat with us
+            </NavLink>
+        </li>
+        <li>
+            <NavLink to="/dashboard/bm-resources" className={`p-2 text-lg rounded-sm`}>
+                <GrResources className="text-3xl" />
+                Business and Marketing Resources
+            </NavLink>
+        </li>
+    </>
+    const adminNavLinks = <>
+        <li className="menu p-0">
+            <details open>
+                <summary className="px-3 rounded-sm"><NavLink to="/dashboard/create-shop" className={` text-lg flex flex-row justify-between items-center gap-2`}>
+                    <HiUserGroup className="text-3xl" />
+                    All Users</NavLink></summary>
+                <ul className="text-lg">
+                    <li><Link to={'/dashboard/seller-panel'}>Seller Panel</Link></li>
+                    <li><Link to={'/dashboard/customer-panel'}>Customer Panel</Link></li>
+                </ul>
+            </details>
+        </li>
+        <li>
+            <NavLink to="/dashboard/product-management" className={`p-2 text-lg rounded-sm`}>
+                <AiOutlineProduct className="text-3xl" />
+                All Products</NavLink>
+        </li>
+        <li>
+            <NavLink to="/dashboard/blogs" className={`p-2 text-lg rounded-sm`}>
+                <TbLogs className="text-3xl" />
+                Selling details
+            </NavLink>
+        </li>
+        <li>
+            <NavLink to="/dashboard/pos" className={`p-2 text-lg rounded-sm`}>
+                <GrAtm className="text-3xl" />
+                Wallet
+            </NavLink>
+        </li>
+
+        <li>
+            <NavLink to="/dashboard/chat-us" className={`p-2 text-lg rounded-sm`}>
+                <FaRegQuestionCircle className="text-3xl" />
+                Seller Request
+            </NavLink>
+        </li>
+        <li>
+            <NavLink to="/dashboard/bm-resources" className={`p-2 text-lg rounded-sm`}>
+                <GrResources className="text-3xl" />
+                Issues And Reports
+            </NavLink>
+        </li>
+
+    </>
     return (
         <div className="">
             <Navbar></Navbar>
@@ -19,42 +104,7 @@ const Dashboard = () => {
                         {
 
                             <ul className="flex md:flex-col">
-                                <li>
-                                    <NavLink to="/dashboard/create-shop" className={`p-2 text-lg rounded-sm`}>
-                                        <MdAddBusiness className="text-3xl" />
-                                        Create a website</NavLink>
-                                </li>
-                                <li>
-                                    <NavLink to="/dashboard/product-management" className={`p-2 text-lg rounded-sm`}>
-                                        <AiOutlineProduct className="text-3xl"/>
-                                        Product Management</NavLink>
-                                </li>
-                                <li>
-                                    <NavLink to="/dashboard/blogs" className={`p-2 text-lg rounded-sm`}>
-                                        <TbLogs className="text-3xl"/>
-                                        Blogs
-                                    </NavLink>
-                                </li>
-                                <li>
-                                    <NavLink to="/dashboard/pos" className={`p-2 text-lg rounded-sm`}>
-                                        <GrAtm className="text-3xl"/>
-                                        POS
-                                    </NavLink>
-                                </li>
-                             
-                                <li>
-                                    <NavLink to="/dashboard/chat-us" className={`p-2 text-lg rounded-sm`}>
-                                        <HiChatBubbleLeftRight className="text-3xl"/>
-                                        Chat with us
-                                    </NavLink>
-                                </li>
-                                <li>
-                                    <NavLink to="/dashboard/bm-resources" className={`p-2 text-lg rounded-sm`}>
-                                        <GrResources className="text-3xl"/>
-                                        Business and Marketing Resources
-                                    </NavLink>
-                                </li>
-
+                                {adminNavLinks}
                             </ul>
 
                         }
@@ -63,7 +113,7 @@ const Dashboard = () => {
                 </div>
                 {/* dashboard content */}
                 <div className="w-full md:col-span-8 lg:col-span-9 ">
-                   
+
                     <Outlet></Outlet>
                 </div>
             </div>
